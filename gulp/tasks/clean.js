73da5af42774d04eb6e2c3b16faf64b4db8f6a96
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2017-02-22 18:07:20 -0500

    remove gulp stuff. add in some deps

--- gulp/tasks/clean.js
+++
@@ -1,4 +0,0 @@
-var gulp    = require('gulp');
-var config  = require('../config').clean;
-
-gulp.task('clean', require('del').bind(null, [config.dest]));
--- gulp/tasks/images.js
+++
@@ -1,15 +0,0 @@
-var gulp         = require('gulp');
-var browserSync  = require('browser-sync');
-var imagemin     = require('gulp-imagemin');
-var config       = require('../config').images;
-
-gulp.task('images', function() { 
-    return gulp.src(config.src) 
-        .pipe(imagemin({
-          progressive: true,
-          interlaced: true,
-          svgoPlugins: [{removeUnknownsAndDefaults: false}, {cleanupIDs: false}]
-        }))
-        .pipe(gulp.dest(config.dest))
-        .pipe(browserSync.stream());
-});
